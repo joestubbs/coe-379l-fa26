5fdeb3ff240e76920362c43119ddbcdb19379a95
Joe Stubbs <joestubbs5@gmail.com>
2026-08-17 16:17:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# COE 379L Fall 2026 Lecture Materials
+
+Course materials for the Fall 2026 instance of COE 379L: Software Design for Responsible Intelligent Systems, 
+UT Austin.
+
+## Building Locally
+
+We are using Nix for the local build. Note that the requirements.txt file is included only for the ReadTheDocs build.
+
+To run the doc engine locally, first enter the Nix development environment
+
+```
+$ nix develop -i 
+```
+
+We recommend the `-i` so that environment variables set in the outside shell don't interfere. 
+In particular, this can prevent issues with locale errors, etc.
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "COE 379L (Fall 2025) Documentation";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    shell-utils.url = "github:waltermoreira/shell-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, shell-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        shell = shell-utils.myShell.${system};
+        python = pkgs.python311;
+        docsPython = python.withPackages (ps: [
+          ps.sphinx
+          ps.sphinx-autobuild
+          ps.sphinx_rtd_theme
+          ps.sphinx-tabs
+          ps.docutils
+          ps.setuptools
+        ]);
+        commonPackages = [ docsPython pkgs.rsync pkgs.gnumake pkgs.lesspipe pkgs.less pkgs.coreutils pkgs.bashInteractive pkgs.which ];
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "379L";
+          buildInputs = commonPackages;
+          shellHook = ''
+           eval "$(lesspipe.sh)"
+           '';
+        };
+        # packages = {
+        #   default = pkgs.stdenv.mkDerivation {
+        #     # builds to ./result
+        #     name = "coe379l";
+        #     src = ./.;
+        #     buildInputs = commonPackages;
+        #     buildPhase = ''
+        #       make html
+        #     '';
+        #     installPhase = ''
+        #       mkdir -p $out
+        #       cp -r build/html $out/
+        #     '';
+        #   };
+        # };
+      }
+    );
+}
